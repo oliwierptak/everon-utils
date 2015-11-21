1f15f4bee9eba37dbfc3f44e73a86869b6003010
Oliwier Ptak <oliwierptak@gmail.com>
2015-11-21 01:37:28 +0100

    text test

--- tests/unit/TextTest.php
+++ tests/unit/TextTest.php
@@ -12,7 +12,7 @@ namespace Everon\Component\Utils\Tests\Unit;
 class TextTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function test_get_data_value()
+    public function test_camel_to_underscore()
     {
 
     }
