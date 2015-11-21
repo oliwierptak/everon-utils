c4aba00f8bd8ad1722ad8826351d7856c952933c
Oliwier Ptak <oliwierptak@gmail.com>
2015-11-21 01:26:22 +0100

    exception tests

---
+++ tests/unit/TextTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the Everon framework.
+ *
+ * (c) Oliwier Ptak <EveronFramework@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Everon\Component\Utils\Tests\Unit;
+
+class TextTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test_get_data_value()
+    {
+
+    }
+
+}
