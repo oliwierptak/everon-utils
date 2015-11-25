2455fafc4ea204321d1e695d7b72c3e573d0179d
Oliwier Ptak <oliwierptak@gmail.com>
2015-11-25 19:07:57 +0100

    composer factoryworker

---
+++ src/FactoryWorker.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of the Everon framework.
+ *
+ * (c) Oliwier Ptak <EveronFramework@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Everon\Component\Utils;
+
+use Everon\Component\Factory\AbstractWorker;
+use Everon\Component\Utils\Popo\Popo;
+
+class FactoryWorker extends AbstractWorker implements FactoryWorkerInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function buildPopo(array $data)
+    {
+        return new Popo($data);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function buildDateTime($time = 'now', \DateTimeZone $timezone = null)
+    {
+        return new \DateTime($time, $timezone);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function buildDateTimeZone($timezone)
+    {
+        return new \DateTimeZone($timezone);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function buildIntlDateFormatter($locale, $datetype, $timetype, $timezone, $calendar, $pattern)
+    {
+        return new \IntlDateFormatter($locale, $datetype, $timetype, $timezone, $calendar, $pattern);
+    }
+}
